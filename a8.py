259e7803776998ddd45c7046c64364d5837dfe3d
Griffin Stofko <gpstofko@cps.edu>
2024-04-22 11:24:26 -0500

    4/22/2024

--- a8.py
+++ a8.py
@@ -2,6 +2,42 @@ from neural import *
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+xor_data = [
+    ([0,0], [0]),
+    ([1,0], [1]),
+    ([0,1], [1]),
+    ([0,0], [0])
+]
+
+xor_nn = NeuralNet(2, 2, 1)
+xor_nn.train(xor_data, iters=100, print_interval=10)
+
+print()
+
+xor_nn1 = NeuralNet(2, 2, 1)
+xor_nn1.train(xor_data, iters=100, print_interval=10)
+
+print()
+
+xor_nn2 = NeuralNet(2, 2, 1)
+xor_nn2.train(xor_data, iters=1000, print_interval=100)
+
+
+print("<<<2>>>")
+
+xor_nn = NeuralNet(2, 8, 1)
+xor_nn.train(xor_data, iters=1000, print_interval=100)
+
+print("<<<3>>>")
+
+xor_nn = NeuralNet(2, 1, 1)
+xor_nn.train(xor_data, iters=1000, print_interval=100)
+
+# print(xor_nn.get_ih_weights())
+# print()
+# print(xor_nn.get_ho_weights())
+
+# print(xor_nn.test_with_expected(xor_data))
 
 
 
